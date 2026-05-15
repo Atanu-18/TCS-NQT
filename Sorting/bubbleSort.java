66d526ab8548be4bfbaaca20f076b84d23d9fdd7
atanu mandal <atanu2697@gmail.com>
2026-05-16 02:33:58 +0530

    sorting

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,34 @@
+public class bubbleSort {
+    public static void main(String args[]){
+
+        int arr[] = {5, 1, 4, 2, 8};
+
+        boolean swapped = false;
+
+        //bubble sort
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                    swapped = true;
+                }
+            }
+            if(swapped == false){
+                break;
+            }
+        }
+
+        printArray(arr);
+    }
+
+    public static void printArray(int arr[]){
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        // System.out.println();
+    }
+}
---
+++ Sorting/selectionSort.java
@@ -0,0 +1,29 @@
+
+public class selectionSort {
+    public static void main(String args[]){
+        int[] arr = {64, 25, 12, 22, 11};
+        
+        //selection sort
+        for(int i = 0; i < arr.length - 1; i++){
+            int smallest = i;
+
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
