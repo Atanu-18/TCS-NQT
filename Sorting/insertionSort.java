09df6e137f25e8db4f0114789e4a31a80f644b37
atanu mandal <atanu2697@gmail.com>
2026-05-17 01:28:01 +0530

    insertion sort done

---
+++ Sorting/insertionSort.java
@@ -0,0 +1,28 @@
+public class insertionSort {
+    public static void main(String args[]){
+        int arr[] = {5,87,3,1,85,20,4};
+
+
+        // insertion sort
+        for(int i = 0; i < arr.length; i++){
+            int current = arr[i];
+
+            int j = i - 1;
+
+            while(j >= 0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = current;
+        }
+
+        printArray(arr);
+    }
+
+    public static void printArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
